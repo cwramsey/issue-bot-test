d4da0dd85b3ed1f2033a97670b189846120895cf
Wenqian Tao <taowenqi@amazon.com>
2019-07-02 18:00:41 -0700

    Add scripts for SAM and Lambda test

---
+++ src/lib/config.ts
@@ -0,0 +1,11 @@
+const userConfig = {
+    "owner": "cwramsey",
+    "repo": "issue-bot-test",
+    "fetchFreq": "1",
+    "issueMaxNoStateTime": "1 weeks",
+    "askForUpdate": "Please update the issue.",
+    "issueMaxWaitTimeToRelease": "1 hours",
+    "closeIssueNote": "Close this issue due to awaiting user information for too long",
+    "askingForChange": "Please change the PR"
+}
+export default userConfig;
