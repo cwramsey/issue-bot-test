5498ef6029ad2b4301350df5ce8b89d01fabe58e
Wenqian Tao <taowenqi@amazon.com>
2019-12-05 17:34:18 -0800

    Change the destination

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,11 @@
 const userConfig = {
-    "owner": "cwramsey",
-    "repo": "issue-bot-test",
-    "fetchFreq": "1000 days",
-    "issueMaxNoStateTime": "1 hours",
+    "owner": "open-mpi",
+    "repo": "ompi-infrastructure-test",
+    "fetchFreq": "1 days",
+    "issueMaxNoStateTime": "1 weeks",
     "askForUpdate": "Please update the issue.",
     "needUpdateFromDeveloper": "Developer, please update the issue.",
-    "issueMaxWaitTimeToRelease": "1 hours",
+    "issueMaxWaitTimeToRelease": "1 weeks",
     "closeIssueNote": "Close this issue due to awaiting user information for too long",
     "askingForChange": "Please change the PR since there is cherry pick not from master",
     "DRYRUN": false
